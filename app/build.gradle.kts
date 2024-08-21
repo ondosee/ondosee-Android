3c32e5a4052824c3826f5d3fdef8b01c0cd65b40
GSM-Minseok <ms0818000@gmail.com>
2024-08-21 09:57:50 +0900

    :sparkles: :: Set app module gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,58 +1,22 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("ondosee.android.application")
+    id("ondosee.android.hilt")
+    id("ondosee.android.firebase")
 }
 
 android {
     namespace = "com.ohnalmwo.ondosee"
-    compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.ohnalmwo.ondosee"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-        }
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/DEPENDENCIES"
         }
     }
 }
 
 dependencies {
-
+    implementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    implementation(libs.androidx.core.splashscreen)
 }
