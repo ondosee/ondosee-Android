805e8fd40089d5f35bc9a84500501d4deb290db9
GSM-Minseok <ms0818000@gmail.com>
2024-08-21 09:57:36 +0900

    :sparkles: :: Fix kotlinCompilerOptions error

--- build-logic/convention/src/main/kotlin/com/ohnalmwo/ondosee/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/ohnalmwo/ondosee/KotlinAndroid.kt
@@ -3,16 +3,14 @@ package com.ohnalmwo.ondosee
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>
@@ -29,8 +27,10 @@ internal fun Project.configureKotlinAndroid(
             targetCompatibility = JavaVersion.VERSION_17
         }
 
-        kotlinCompilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+        extensions.getByType<KotlinAndroidProjectExtension>().apply {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_17)
+            }
         }
     }
 }
@@ -52,8 +52,4 @@ private fun Project.configureJvm() {
             freeCompilerArgs.set(listOf("-Xjsr305=strict"))
         }
     }
-}
-
-fun CommonExtension<*, *, *, *, *, *>.kotlinCompilerOptions(block: KotlinJvmCompilerOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinCompilerOptions", block)
 }
