cc079a575f34cdf599eebf6b920eb279e75bc7f4
GSM-Minseok <ms0818000@gmail.com>
2024-08-21 14:07:19 +0900

    :memo: :: Add data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,22 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("goms.android.core")
+    id("goms.android.hilt")
+}
+
+android {
+    namespace = "com.ohnalmwo.data"
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:model"))
+    implementation(project(":core:network"))
+    implementation(project(":core:datastore"))
+
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+}
