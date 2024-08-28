f39d4031af6fe6b4abd523458b47c9fd5c16ec10
GSM-Minseok <ms0818000@gmail.com>
2024-08-28 10:09:01 +0900

    :sparkles: :: Add ColorTheme

---
+++ core/design-system/src/main/java/com/ohnalmwo/design_system/theme/ColorTheme.kt
@@ -0,0 +1,31 @@
+package com.goms.design_system.theme
+
+import androidx.compose.ui.graphics.Color
+import com.ohnalmwo.design_system.theme.color.BackgroundColor
+
+abstract class ColorTheme {
+    //System Colors
+    abstract val PRIMARY: Color
+    abstract val WARNING: Color
+    abstract val ABLE: Color
+    abstract val DISABLE: Color
+    abstract val SECONDARY: Color
+    abstract val TERTIARY: Color
+
+    //Component Colors
+    abstract val RAIN: Color
+    abstract val SNOW: Color
+    abstract val VERY_GOOD: Color
+    abstract val SO_GOOD: Color
+    abstract val GOOD: Color
+    abstract val SOSO: Color
+    abstract val BAD: Color
+    abstract val SO_BAD: Color
+    abstract val TOO_BAD: Color
+    abstract val WORST: Color
+
+    abstract val BLACK: Color
+    abstract val WHITE: Color
+
+    abstract val BACKGROUND: Color
+}
---
+++ core/design-system/src/main/java/com/ohnalmwo/design_system/theme/color/LightColor.kt
@@ -0,0 +1,30 @@
+package com.ohnalmwo.design_system.theme.color
+
+import androidx.compose.ui.graphics.Color
+import com.goms.design_system.theme.ColorTheme
+import com.ohnalmwo.design_system.theme.color.BackgroundColor
+
+object LightColor : ColorTheme() {
+    override val PRIMARY = Color(0xFF00BFFF)
+    override val WARNING = Color(0xFFFF3333)
+    override val ABLE = Color(0xFF26D926)
+    override val DISABLE = Color(0xFFCCCCCC)
+    override val SECONDARY = Color(0xBFFFFFFF)
+    override val TERTIARY = Color(0x80FFFFFF)
+
+    override val RAIN = Color(0xFF33CCFF)
+    override val SNOW = Color(0xFFFFFFFF)
+    override val VERY_GOOD = Color(0xFF4D97FF)
+    override val SO_GOOD = Color(0xFF4DD2FF)
+    override val GOOD = Color(0xFF4DFFC3)
+    override val SOSO = Color(0xFF66FF66)
+    override val BAD = Color(0xFFFFD24D)
+    override val SO_BAD = Color(0xFFFFA64D)
+    override val TOO_BAD = Color(0xFFFF794D)
+    override val WORST = Color(0xFF262626)
+
+    override val BLACK = Color(0xFF000000)
+    override val WHITE = Color(0xFFFFFFFF)
+
+    override var BACKGROUND = Color(0xFFF9FAFA)
+}
