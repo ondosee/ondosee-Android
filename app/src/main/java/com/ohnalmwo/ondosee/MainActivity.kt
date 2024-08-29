6e43f8248f7565bb258dbbc0a8af603cdb93e0f6
송현서 <s22026@gsm.hs.kr>
2024-08-29 17:30:42 +0900

    :sparkles: :: Add setSystemBarsColor

--- app/src/main/java/com/ohnalmwo/ondosee/MainActivity.kt
+++ app/src/main/java/com/ohnalmwo/ondosee/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ohnalmwo.ondosee.ui.OndoseeApp
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +15,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
+
+            DisposableEffect(systemUiController, useDarkIcons) {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons,
+                    isNavigationBarContrastEnforced = false
+                )
+
+                onDispose {}
+            }
+
             OndoseeApp()
         }
     }
