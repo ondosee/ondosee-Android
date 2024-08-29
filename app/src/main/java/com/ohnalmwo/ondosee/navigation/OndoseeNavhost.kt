51907485b03fa3642c6a6b16a86660ab73dc14cf
송현서 <s22026@gsm.hs.kr>
2024-08-29 17:30:24 +0900

    :sparkles: :: Add statusBarsPadding

--- app/src/main/java/com/ohnalmwo/ondosee/navigation/OndoseeNavhost.kt
+++ app/src/main/java/com/ohnalmwo/ondosee/navigation/OndoseeNavhost.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,7 +41,8 @@ fun OndoseeNavHost(
                             colors = colors.BACKGROUND_RAIN
                         )
                     )
-            ) {
+                    .statusBarsPadding()
+                ) {
                 LazyColumn {
                     items(10) {
                         Box(
