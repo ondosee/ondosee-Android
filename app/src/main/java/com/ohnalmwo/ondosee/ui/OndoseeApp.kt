da2997d9ef43a810dffc171322ecd4a3781d54d5
송현서 <s22026@gsm.hs.kr>
2024-08-29 11:58:58 +0900

    :heavy_plus_sign: :: Apply haze

--- app/src/main/java/com/ohnalmwo/ondosee/ui/OndoseeApp.kt
+++ app/src/main/java/com/ohnalmwo/ondosee/ui/OndoseeApp.kt
@@ -1,61 +1,36 @@
 package com.ohnalmwo.ondosee.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.rememberNavController
 import com.ohnalmwo.design_system.component.bottombar.BottomNavigationBar
 import com.ohnalmwo.design_system.theme.OndoseeTheme
-import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
 import com.ohnalmwo.model.enum.ThemeType
 import com.ohnalmwo.ondosee.navigation.OndoseeNavHost
+import dev.chrisbanes.haze.HazeState
 
 @Composable
 fun OndoseeApp() {
 
     val navController = rememberNavController()
+    val hazeState = remember { HazeState() }
 
     OndoseeTheme(themeMode = ThemeType.SYSTEM) {
         CompositionLocalProvider {
             Scaffold(
                 bottomBar = {
-                    BottomNavigationBar(navController = navController)
+                    BottomNavigationBar(
+                        hazeState = hazeState,
+                        navController = navController
+                    )
                 }
-            ) { innerPadding ->
-                OndoseeNavHost(navController = navController)
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .height(600.dp)
-                        .padding(innerPadding)
-                        .background(
-                            brush = Brush.linearGradient(
-                                colors = colors.BACKGROUND_RAIN
-                            )
-                        )
-                ) {
-                    LazyColumn {
-                        items(10) {
-                            Box(
-                                modifier = Modifier
-                                    .size(100.dp)
-                                    .background(colors.BLACK)
-                            )
-                            Spacer(modifier = Modifier.size(50.dp))
-                        }
-                    }
-                }
+            ) { _ ->
+                OndoseeNavHost(
+                    hazeState = hazeState,
+                    navController = navController
+                )
             }
         }
     }
--- core/design-system/src/main/java/com/ohnalmwo/design_system/component/bottombar/BottomNavigationBar.kt
+++ core/design-system/src/main/java/com/ohnalmwo/design_system/component/bottombar/BottomNavigationBar.kt
@@ -4,7 +4,7 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +22,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
@@ -31,10 +34,14 @@ import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
 import com.ohnalmwo.design_system.theme.OndoseeTheme.typography
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.hazeChild
 
 @Composable
 fun BottomNavigationBar(
     modifier: Modifier = Modifier,
+    hazeState: HazeState,
     navController: NavController,
 ) {
     val bottomBarItems = listOf(
@@ -49,7 +56,23 @@ fun BottomNavigationBar(
         modifier = modifier
             .fillMaxWidth()
             .height(60.dp)
-            .background(color = colors.BLACK)
+            .hazeChild(
+                state = hazeState,
+                shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+                style = HazeStyle(
+                    tint = colors.WHITE.copy(.2f),
+                )
+            )
+            .border(
+                width = Dp.Hairline,
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        colors.WHITE.copy(alpha = .8f),
+                        colors.WHITE.copy(alpha = .2f),
+                    ),
+                ),
+                shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)
+            )
     ) {
         bottomBarItems.forEach { bottomBarItem ->
             val isSelected = currentDestination?.hierarchy?.any {
