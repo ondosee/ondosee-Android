ab4c548af8031e327daf2f84d2054102f53d038d
송현서 <s22026@gsm.hs.kr>
2024-08-29 11:22:52 +0900

    :sparkles: :: Add BottomBarItem

---
+++ core/design-system/src/main/java/com/ohnalmwo/design_system/component/bottombar/BottomBarItem.kt
@@ -0,0 +1,10 @@
+package com.ohnalmwo.design_system.component.bottombar
+
+import com.ohnalmwo.design_system.R
+import com.ohnalmwo.model.enum.Route
+
+sealed class BottomBarItem(val title: String, val icon: Int, val route: Route) {
+    data object Main : BottomBarItem("대시보드", R.drawable.ic_main, Route.Main)
+    data object Weekly : BottomBarItem("주간예보", R.drawable.ic_weekly, Route.Weekly)
+    data object Setting : BottomBarItem("설정", R.drawable.ic_setting, Route.Setting)
+}
