6dd29936316f3de6d0e056151601b45911fb7ad0
송현서 <s22026@gsm.hs.kr>
2024-09-02 13:32:58 +0900

    :lipstick: :: Add LocationIndicator

---
+++ core/design-system/src/main/java/com/ohnalmwo/design_system/component/topbar/LocationIndicator.kt
@@ -0,0 +1,63 @@
+package com.ohnalmwo.design_system.component.topbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ohnalmwo.design_system.icons.CurrentLocationIcon
+import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
+
+@Composable
+fun LocationIndicator(
+    modifier: Modifier = Modifier,
+    totalNumber: Int,
+    currentLocation: Int,
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            modifier = modifier.height(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            repeat(totalNumber) { index ->
+                LocationDot(
+                    modifier = Modifier.size(12.dp),
+                    isSelected = index == currentLocation,
+                    isFirstDot = index == 0
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LocationDot(
+    modifier: Modifier,
+    isSelected: Boolean,
+    isFirstDot: Boolean
+) {
+    val color = if (isSelected) colors.PRIMARY else colors.SECONDARY
+
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        if (isFirstDot) {
+            CurrentLocationIcon(tint = color)
+        } else {
+            Box(
+                modifier = Modifier
+                    .size(6.dp)
+                    .background(color = color, shape = CircleShape)
+            )
+        }
+    }
+}
