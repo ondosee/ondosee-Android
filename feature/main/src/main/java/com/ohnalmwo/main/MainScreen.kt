b1f9d07c7977e008572750d06c29b16bc08c78ae
송현서 <s22026@gsm.hs.kr>
2024-09-03 14:54:15 +0900

    :lipstick: :: Create MainScreen

---
+++ feature/main/src/main/java/com/ohnalmwo/main/MainScreen.kt
@@ -0,0 +1,96 @@
+package com.ohnalmwo.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import com.ohnalmwo.design_system.component.lottie.AnimatedLottie
+import com.ohnalmwo.design_system.component.topbar.OndoseeTopBar
+import com.ohnalmwo.design_system.icons.MenuIcon
+import com.ohnalmwo.main.component.SignificantWeatherText
+import com.ohnalmwo.main.component.WeatherConditionDescriptionText
+import com.ohnalmwo.main.component.WeatherConditionText
+import com.ohnalmwo.main.component.WeatherForecastCard
+import com.ohnalmwo.model.enum.BackgroundType
+import com.ohnalmwo.ui.getBackgroundColors
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.haze
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun MainScreen(
+    hazeState: HazeState
+) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = getBackgroundColors(type = BackgroundType.RAIN)
+                )
+            )
+            .haze(state = hazeState)
+            .statusBarsPadding()
+    ) {
+        OndoseeTopBar {
+            MenuIcon()
+        }
+        MotionLayout(
+            modifier = Modifier.fillMaxWidth(),
+            motionScene = MotionScene(content = motionScene),
+        ) {
+            SignificantWeatherText(
+                modifier = Modifier.layoutId("significantWeatherText"),
+                text = "90%",
+                backgroundType = BackgroundType.RAIN
+            )
+            AnimatedLottie(
+                modifier = Modifier.layoutId("animatedLottie"),
+                rawId = com.ohnalmwo.design_system.R.raw.rainy
+            )
+            WeatherConditionText(
+                modifier = Modifier.layoutId("weatherConditionText"),
+                text = "비가 올 것 같아요!"
+            )
+            WeatherConditionDescriptionText(
+                modifier = Modifier.layoutId("weatherConditionDescriptionText"),
+                text = "우산을 미리 챙겨주세요!"
+            )
+            WeatherForecastCard(
+                modifier = Modifier.layoutId("weatherForecastCardFirst"),
+                description = "오전 3시부터 5시간 동안 비가 올 예정이에요.\n" +
+                        "우산을 가져가세요."
+            )
+            WeatherForecastCard(
+                modifier = Modifier.layoutId("weatherForecastCardSecond"),
+                description = "오후 5시부터 9시간 동안 미세먼지/꽂가루가 심해요.\n" +
+                        "마스크를 가져가세요."
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun MainScreenPreview() {
+    //MainScreen()
+}
