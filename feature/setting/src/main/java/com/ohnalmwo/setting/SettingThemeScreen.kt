46dfb4975066b294e1f49ba86a76819ac7bb1e79
GSM-Minseok <ms0818000@gmail.com>
2024-09-23 15:03:42 +0900

    :lipstick: :: Add SettingThemeScreen.kt

---
+++ feature/setting/src/main/java/com/ohnalmwo/setting/SettingThemeScreen.kt
@@ -0,0 +1,36 @@
+package com.ohnalmwo.setting
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
+import com.ohnalmwo.setting.component.SettingBackButton
+import com.ohnalmwo.setting.component.SettingOptionTitle
+import com.ohnalmwo.setting.component.SettingSelectComponent
+import com.ohnalmwo.setting.component.SettingTitle
+import kotlinx.collections.immutable.toPersistentList
+
+@Composable
+fun SettingThemeScreen(
+    onBackClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .background(color = colors.BACKGROUND)
+    ) {
+        SettingBackButton(modifier = Modifier.padding(top = 24.dp)) { onBackClick() }
+        SettingTitle(modifier = Modifier.padding(top = 16.dp), title = "테마 설정")
+        SettingOptionTitle(modifier = Modifier.padding(top = 24.dp), optionName = "기본 테마")
+        SettingSelectComponent(modifier = Modifier.padding(horizontal = 20.dp), dropdownList = listOf("시스템 테마 설정","다크","화이트").toPersistentList(), selectedIndex = 2)
+    }
+}
