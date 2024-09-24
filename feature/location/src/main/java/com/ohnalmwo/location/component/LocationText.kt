57191fa731fe5095a37d00e0bbe74e497a2bb110
송현서 <s22026@gsm.hs.kr>
2024-09-25 08:40:26 +0900

    :lipstick: :: Create LocationText

---
+++ feature/location/src/main/java/com/ohnalmwo/location/component/LocationText.kt
@@ -0,0 +1,22 @@
+package com.ohnalmwo.location.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
+import com.ohnalmwo.design_system.theme.OndoseeTheme.typography
+
+@Composable
+fun LocationText(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Text(
+        text = text,
+        style = typography.titleMedium,
+        fontWeight = FontWeight.Bold,
+        color = colors.WHITE,
+        modifier = modifier
+    )
+}
