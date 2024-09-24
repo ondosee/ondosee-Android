6b52726e04d26eec233a1a85551121dc3e1cc707
송현서 <s22026@gsm.hs.kr>
2024-09-24 20:03:29 +0900

    :sparkles: :: Distinguish between bottom bar markings

--- app/src/main/java/com/ohnalmwo/ondosee/ui/OndoseeApp.kt
+++ app/src/main/java/com/ohnalmwo/ondosee/ui/OndoseeApp.kt
@@ -3,10 +3,13 @@ package com.ohnalmwo.ondosee.ui
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ohnalmwo.ui.BottomNavigationBar
 import com.ohnalmwo.design_system.theme.OndoseeTheme
+import com.ohnalmwo.model.enum.Route
 import com.ohnalmwo.model.enum.ThemeType
 import com.ohnalmwo.ondosee.navigation.OndoseeNavHost
 import dev.chrisbanes.haze.HazeState
@@ -20,12 +23,23 @@ fun OndoseeApp() {
 
     OndoseeTheme(themeMode = ThemeType.SYSTEM) {
         CompositionLocalProvider {
+            val currentRoute by navController.currentBackStackEntryAsState()
+            val showBottomBar = remember(currentRoute) {
+                currentRoute?.destination?.route in setOf(
+                    Route.Main.toString(),
+                    Route.Weekly.toString(),
+                    Route.Setting.toString()
+                )
+            }
+
             Scaffold(
                 bottomBar = {
-                    BottomNavigationBar(
-                        hazeState = hazeState,
-                        navController = navController
-                    )
+                    if (showBottomBar) {
+                        BottomNavigationBar(
+                            hazeState = hazeState,
+                            navController = navController
+                        )
+                    }
                 }
             ) { _ ->
                 OndoseeNavHost(
