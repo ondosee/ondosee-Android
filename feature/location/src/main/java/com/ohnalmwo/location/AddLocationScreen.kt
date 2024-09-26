a30c5ffd2ad74794a2f0c9057d110b723feb3cac
송현서 <s22026@gsm.hs.kr>
2024-09-27 08:54:51 +0900

    :lipstick: :: Create AddLocationScreen

---
+++ feature/location/src/main/java/com/ohnalmwo/location/AddLocationScreen.kt
@@ -0,0 +1,47 @@
+package com.ohnalmwo.location
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ohnalmwo.design_system.component.textfield.SearchTextField
+import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
+import com.ohnalmwo.location.component.LocationList
+import com.ohnalmwo.location.component.LocationText
+
+@Composable
+fun AddLocationScreen() {
+    var location by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colors.BACKGROUND)
+            .statusBarsPadding()
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        LocationText(text = "위치 추가하기")
+        SearchTextField(
+            placeHolder = "도시 또는 공항 검색",
+            setText = location,
+            singleLine = true
+        ) {
+            location = it
+        }
+        LocationList(
+            searchQuery = location,
+            locations = listOf("경기도 광주시", "광주광역시 동구", "광주광역시 남구", "광주광역시 광산구", "광주광역시 북구")
+        ) {}
+    }
+}
