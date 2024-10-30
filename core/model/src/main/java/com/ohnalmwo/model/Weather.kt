15685cf0fb9fd9f93bce90150b1c7b6cdadc3a9c
송현서 <s22026@gsm.hs.kr>
2024-10-30 09:18:11 +0900

    :sparkles: :: Add default function

--- core/model/src/main/java/com/ohnalmwo/model/Weather.kt
+++ core/model/src/main/java/com/ohnalmwo/model/Weather.kt
@@ -7,16 +7,36 @@ import kotlinx.datetime.LocalTime
 @Immutable
 data class Weather(
     val weathers: List<WeatherDetail>
-)
+) {
+    companion object {
+        fun default() = Weather(
+            weathers = listOf(WeatherDetail.default())
+        )
+    }
+}
 
 @Immutable
 data class WeatherDetail(
     val significant: Significant,
     val timeZone: List<TimeZoneInfo>
-)
+) {
+    companion object {
+        fun default() = WeatherDetail(
+            significant = Significant.BEST_10,
+            timeZone = listOf(TimeZoneInfo.default())
+        )
+    }
+}
 
 @Immutable
 data class TimeZoneInfo(
     val time: LocalTime,
     val value: String
-)
+) {
+    companion object {
+        fun default() = TimeZoneInfo(
+            time = LocalTime(0, 0),
+            value = ""
+        )
+    }
+}
