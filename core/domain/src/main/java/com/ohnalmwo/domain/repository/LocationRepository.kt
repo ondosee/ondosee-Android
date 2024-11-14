82a90ec788518ff25f9ef6f5098810d4e0e614b1
송현서 <s22026@gsm.hs.kr>
2024-11-14 14:54:13 +0900

    :sparkles: :: Add local locations function to LocationRepository

--- core/domain/src/main/java/com/ohnalmwo/domain/repository/LocationRepository.kt
+++ core/domain/src/main/java/com/ohnalmwo/domain/repository/LocationRepository.kt
@@ -1,8 +1,17 @@
 package com.ohnalmwo.domain.repository
 
 import com.ohnalmwo.model.Location
+import com.ohnalmwo.model.LocationInfo
 import kotlinx.coroutines.flow.Flow
 
 interface LocationRepository {
     fun getLocationCoordinate(keyword: String, page: Int): Flow<Location>
+
+    fun getLocations(): Flow<List<LocationInfo>>
+
+    suspend fun setLocations(location: LocationInfo)
+
+    suspend fun removeLocations(index: Int)
+
+    suspend fun removeAllLocations()
 }
