6588dd14418cdec2cd2a118ea757d957d86949ca
송현서 <s22026@gsm.hs.kr>
2024-11-14 09:55:35 +0900

    :sparkles: :: Create DataStoreModule

---
+++ core/datastore/src/main/java/com/ohnalmwo/datastore/di/DataStoreModule.kt
@@ -0,0 +1,30 @@
+package com.ohnalmwo.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.dataStoreFile
+import com.ohnalmwo.datastore.Locations
+import com.ohnalmwo.datastore.serializer.LocationsSerializer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideLocationsDataStore(
+        @ApplicationContext context: Context,
+        locationsSerializer: LocationsSerializer
+    ): DataStore<Locations> =
+        DataStoreFactory.create(
+            serializer = locationsSerializer,
+        ) {
+            context.dataStoreFile("locations.pb")
+        }
+}
