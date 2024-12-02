652319bf647b60df2b8749a0182fa890daa215f4
GSM-Minseok <ms0818000@gmail.com>
2024-12-02 16:50:41 +0900

    :lipstick: :: Reflection of design changes to TodayTemperature

--- feature/weekly-weather/src/main/java/com/minstone/weekly_weather/component/TodayTemperature.kt
+++ feature/weekly-weather/src/main/java/com/minstone/weekly_weather/component/TodayTemperature.kt
@@ -1,19 +1,12 @@
 package com.minstone.weekly_weather.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.ohnalmwo.design_system.theme.OndoseeTheme.colors
@@ -28,55 +21,35 @@ fun TodayTemperature(
     minTemperature: Int,
     maxTemperature: Int
 ) {
-    Box(
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(top = 20.dp, bottom = 16.dp)
-    ) {
-        Temperature(
-            modifier = Modifier,
-            minTemperature = minTemperature,
-            maxTemperature = maxTemperature
-        )
-
-        NowTemperature(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .offset(y = (-16).dp),
-            nowTemperature = nowTemperature
-        )
-    }
-}
-
-@Composable
-fun NowTemperature(
-    modifier: Modifier,
-    nowTemperature: Int
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(top = 4.dp, bottom = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            modifier = Modifier.padding(bottom = 4.dp),
+            modifier = Modifier,
             text = nowTemperature.toString().appendCelsiusSymbol(),
-            style = typography.textSmall,
+            style = typography.titleLarge,
             color = colors.THEME_WHITE,
             fontWeight = FontWeight.Bold
         )
 
-        Box(
-            modifier = Modifier
-                .size(12.dp)
-                .background(colors.BACKGROUND_SNOW[0]),
-            contentAlignment = Alignment.Center
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(6.dp)
-                    .clip(CircleShape)
-                    .background(colors.THEME_WHITE)
+        Row {
+            Text(
+                modifier = Modifier.padding(end = 4.dp),
+                text = "최저: " + minTemperature.toString().appendCelsiusSymbol(),
+                style = typography.textMedium,
+                color = colors.THEME_WHITE.copy(alpha = 0.75f),
+                fontWeight = FontWeight.Normal
+            )
+
+            Text(
+                modifier = Modifier.padding(end = 16.dp),
+                text = "최고: " + maxTemperature.toString().appendCelsiusSymbol(),
+                style = typography.textMedium,
+                color = colors.THEME_WHITE.copy(alpha = 0.75f),
+                fontWeight = FontWeight.Normal
             )
         }
     }
