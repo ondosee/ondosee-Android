5b9d7b7c3df673334051bccef36412495169bfc9
GSM-Minseok <ms0818000@gmail.com>
2024-12-11 13:49:33 +0900

    :sparkles: :: Add SettingDataSource

---
+++ core/datastore/src/main/java/com/ohnalmwo/datastore/datasource/setting/SettingDataSource.kt
@@ -0,0 +1,11 @@
+package com.ohnalmwo.datastore.datasource.setting
+
+import com.ohnalmwo.model.LocationInfo
+import com.ohnalmwo.model.enum.Switch
+import kotlinx.coroutines.flow.Flow
+
+interface SettingDataSource {
+    suspend fun setAlarmState(alarmState: String)
+
+    fun getAlarmState(): Flow<Switch>
+}
---
+++ core/datastore/src/main/java/com/ohnalmwo/datastore/datasource/setting/SettingDataSourceImpl.kt
@@ -0,0 +1,35 @@
+package com.ohnalmwo.datastore.datasource.setting
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import com.ohnalmwo.datastore.Locations
+import com.ohnalmwo.datastore.SettingInfo
+import com.ohnalmwo.datastore.mapper.toData
+import com.ohnalmwo.datastore.mapper.toDomain
+import com.ohnalmwo.model.LocationInfo
+import com.ohnalmwo.model.enum.Switch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class SettingDataSourceImpl @Inject constructor (
+    private val settingInfo: DataStore<SettingInfo>
+) : SettingDataSource {
+    override suspend fun setAlarmState(alarmState: String) {
+        settingInfo.updateData {
+            it.toBuilder()
+                .setAlarm(alarmState)
+                .build()
+        }
+    }
+
+    override fun getAlarmState(): Flow<Switch> =
+        settingInfo.data.map {
+            Log.d("testt", it.alarm)
+            when (it.alarm) {
+                Switch.ON.value -> Switch.ON
+                Switch.OFF.value -> Switch.OFF
+                else -> Switch.OFF
+            }
+        }
+}
