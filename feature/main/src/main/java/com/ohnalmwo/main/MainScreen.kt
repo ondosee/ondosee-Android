ca010927536bb666c27e5da1797f1f90bfb54506
송현서 <s22026@gsm.hs.kr>
2025-01-07 23:04:01 +0900

    :sparkles: :: Import users current location

--- feature/main/src/main/java/com/ohnalmwo/main/MainScreen.kt
+++ feature/main/src/main/java/com/ohnalmwo/main/MainScreen.kt
@@ -1,5 +1,9 @@
 package com.ohnalmwo.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -8,8 +12,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.core.app.ActivityCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.google.android.gms.location.LocationServices
 import com.ohnalmwo.main.component.LoadingComponent
 import com.ohnalmwo.main.component.MainComponent
 import com.ohnalmwo.main.component.TutorialDialog
@@ -18,6 +28,7 @@ import com.ohnalmwo.main.viewmodel.MainViewModel
 import com.ohnalmwo.ui.rememberFlowWithLifecycle
 import dev.chrisbanes.haze.HazeState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainRoute(
     hazeState: HazeState,
@@ -26,9 +37,26 @@ fun MainRoute(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
     val effect = rememberFlowWithLifecycle(viewModel.effect)
+    val findLocationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+    val context = LocalContext.current
+    val locationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+    var userLocation by remember { mutableStateOf<Pair<Double, Double>?>(null) }
 
     LaunchedEffect(Unit) {
-        viewModel.getWeatherSignificant(x = 126.85250, y = 35.15944)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    userLocation = it.latitude to it.longitude
+                    viewModel.getWeatherSignificant(x = it.longitude, y = it.latitude)
+                }
+            }
+        } else {
+            findLocationPermissionState.launchPermissionRequest()
+        }
         viewModel.getSavedLocations()
         viewModel.getTutorialDialogState()
     }
@@ -42,6 +70,7 @@ fun MainRoute(
     }
 
     MainScreen(
+        context = context,
         hazeState = hazeState,
         state = state,
         onDismissClick = viewModel::setTutorialDialogState,
@@ -51,12 +80,12 @@ fun MainRoute(
 
 @Composable
 fun MainScreen(
+    context: Context,
     hazeState: HazeState,
     state: MainState,
     onDismissClick: (Boolean) -> Unit,
     navigateToLocation: () -> Unit
 ) {
-    val context = LocalContext.current
     val motionScene = remember {
         context.resources
             .openRawResource(R.raw.motion_scene)
